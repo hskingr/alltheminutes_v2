5419c5d1eef7071cf90e2411bc186256b4fbf3ac
me <me>
2022-09-03 12:21:15 +0100

    correct time

--- functions/getTweets.js
+++ functions/getTweets.js
@@ -1,9 +1,12 @@
 import { lightFormat } from 'date-fns';
+import pkg from 'date-fns-tz';
+const { formatInTimeZone } = pkg;
 
 export default async function getTweets(twitterClient) {
   try {
-    const twentyFourHourTime = lightFormat(new Date(), `HH:mm`);
-    const twelveHourTime = lightFormat(new Date(), `h:mm`);
+    const date = new Date();
+    const twentyFourHourTime = formatInTimeZone(date, 'Europe/London', 'HH:mm');
+    const twelveHourTime = formatInTimeZone(date, 'Europe/London', 'h:mm');
     const amPm = lightFormat(new Date(), `a`);
     const query = `("It's ${twentyFourHourTime} and" OR "It's ${twelveHourTime}${amPm} and" OR "It's ${twelveHourTime} ${amPm} and") lang:en -is:retweet`;
     const results = await twitterClient.v2.search(query);
